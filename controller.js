71b9d15ebd040b150546d268b5f62b1913d32608
iZhGH <174579759+iZhGH@users.noreply.github.com>
2025-03-09 21:30:47 -0700

    fix+feat(index.html + javascript.js): Flesh out projects - FIX: Put new .js file title in HTML link - Add getter + setter for projects module

--- controller.js
+++ controller.js
@@ -1,5 +1,17 @@
 const projects = (function() {
+    let projectList = ["all"];
 
+    function getProjects() {
+        return projectList;
+    }
+
+    function addProjects(projectName) {
+        projectList.push(projectName);
+    }
+
+
+
+    return {getProjects, addProjects}
 })();
  
 class toDo {
