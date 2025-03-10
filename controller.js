1469a7701869fcd424a205ab66c9adda5140cdb1
iZhGH <174579759+iZhGH@users.noreply.github.com>
2025-03-09 21:26:21 -0700

    feat(javascript.js): module/class - Create project IIFE module - Create toDo class

---
+++ controller.js
@@ -0,0 +1,13 @@
+const projects = (function() {
+
+})();
+ 
+class toDo {
+    constructor(title, description, dueDate, priority) {
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.priority = priority;
+    }
+
+}
