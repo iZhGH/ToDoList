a0e29db0932d233a5d18e4f866910c2c7bfd32e1
iZhGH <174579759+iZhGH@users.noreply.github.com>
2025-03-09 22:46:03 -0700

    feat(javascript.js): Add button functionality - Create functionality so add button press will start prompts to create new ToDo element - Add checks in addToDo() to validate and restrict user input.

--- controller.js
+++ controller.js
@@ -3,8 +3,6 @@ let addBtn = document.querySelector(".addButton");
 // Grab parent element for ToDos (the board)
 
 
-// addBtn.addEventListener('click', toDoHandler.addToDo());
-
 const projects = (function() {
     let projectList = ["All"];
 
@@ -28,13 +26,29 @@ class toDo {
         this.description = description;
         this.dueDate = dueDate;
         this.Priority = Priority;
-        this.includedIn = includedIn;
+        this.includedIn = ["all"];
     }
+
 }
  
 const toDoHandler = (function() {
+    let datePattern = /^\d{4}-\d{2}-\d{2}$/;  // Regex to match 'YYYY-MM-DD'
     function addToDo() {
         // Ask a bunch of questions
+        let name = prompt("Enter a name for your new task");
+        let description = prompt("Give a quick description for your task");
+        let dueDate = prompt("Please enter a due date for this task (YYYY-MM-DD");
+        console.log(datePattern.test(dueDate));
+        while (!datePattern.test(dueDate)) {
+            dueDate = prompt("ERROR! --- > Please enter a due date for this task (YYYY-MM-DD");
+        }
+        let priorty = prompt("What is the priority level for this task? 1 = Urgent, 2 = Moderate, 3 = None");
+        while (parseInt(priorty) != 1 && parseInt(priorty) != 2 && parseInt(priorty) != 3) {
+            priorty = prompt("ERROR! INVALID INPUT --- > What is the priority level for this task? 1 = Urgent, 2 = Moderate, 3 = None");
+        }
+        let includedin = prompt("Would you like to add this task to a specific project? If so enter the name of the project. (If it doesn't exist a new one will be created!) If you don't want to, press ENTER, leaving the field blank");
+        // Prompt validation
+
         // Push answers into toDo array as object with K/V's
         // Add new project to projects list (if given)
         // Render ToDo's. render.renderToDo();
@@ -71,3 +85,4 @@ const render = (function() {
 
 //projects.addProjects("Training");
 render.renderProjects();
+addBtn.addEventListener('click', toDoHandler.addToDo);
